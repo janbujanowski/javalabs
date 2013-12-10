05f7f63da8fd2086e298e863ebe6bd4bc46b6e35
Mikołaj Olszewski <mikus156@gmail.com>
2013-12-10 10:40:31 +0100

    Domyślne wartości w LabDescriptor zmienione na "..."

--- src/main/java/pk/labs/LabA/LabDescriptor.java
+++ src/main/java/pk/labs/LabA/LabDescriptor.java
@@ -3,19 +3,19 @@ package pk.labs.LabA;
 public class LabDescriptor {
 
     // region P1
-    public static String displayImplClassName = null;
-    public static String controlPanelImplClassName = null;
+    public static String displayImplClassName = "...";
+    public static String controlPanelImplClassName = "...";
 
-    public static String mainComponentSpecClassName = null;
-    public static String mainComponentImplClassName = null;
+    public static String mainComponentSpecClassName = "...";
+    public static String mainComponentImplClassName = "...";
     // endregion
 
     // region P2
-    public static String mainComponentBeanName = null;
-    public static String mainFrameBeanName = null;
+    public static String mainComponentBeanName = "...";
+    public static String mainFrameBeanName = "...";
     // endregion
 
     // region P3
-    public static String sillyFrameBeanName = null;
+    public static String sillyFrameBeanName = "...";
     // endregion
 }

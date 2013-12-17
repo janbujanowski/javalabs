6f1411ed80d6b299616306c6ed3df80ed090068a
Mikołaj Olszewski <mikus156@gmail.com>
2013-12-17 08:47:36 +0100

    wrapper

--- src/main/java/pk/labs/LabB/LabDescriptor.java
+++ src/main/java/pk/labs/LabB/LabDescriptor.java
@@ -3,15 +3,22 @@ package pk.labs.LabB;
 import pk.labs.LabB.Contracts.*;
 
 public class LabDescriptor {
+
+    // region P1
     public static String displayImplClassName = "...";
     public static String controlPanelImplClassName = "...";
 
     public static String mainComponentSpecClassName = "...";
     public static String mainComponentImplClassName = "...";
     public static String mainComponentBeanName = "...";
-    
+    // endregion
+
+    // region P2
     public static String mainComponentMethodName = "...";
     public static Object[] mainComponentMethodExampleParams = new Object[] { };
-    
+    // endregion
+
+    // region P3
     public static String loggerAspectBeanName = "...";
+    // endregion
 }

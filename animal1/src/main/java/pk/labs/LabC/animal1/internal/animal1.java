a6b453414662af78799827c50bf184b04b0506b6
jbujanowski <jan.bujanowski@hotmail.com>
2015-01-19 21:48:01 +0100

    P1 Full

---
+++ animal1/src/main/java/pk/labs/LabC/animal1/internal/animal1.java
@@ -0,0 +1,43 @@
+package pk.labs.LabC.animal1.internal;
+
+import pk.labs.LabC.contracts.Animal;
+
+import java.beans.PropertyChangeListener;
+
+/**
+ * Created by Jan on 2015-01-19.
+ */
+public class animal1 implements Animal {
+
+
+    @Override
+    public String getSpecies() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getStatus() {
+        return null;
+    }
+
+    @Override
+    public void setStatus(String status) {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+    }
+
+}
